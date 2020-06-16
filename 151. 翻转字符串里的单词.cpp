937307fa3a6714ca42d43cbd07cb9153b65cea0f
Cdreamfly <2290660986@qq.com>
2020-06-16 20:55:01 +0800

    151

---
+++ 151. 翻转字符串里的单词.cpp
@@ -0,0 +1,20 @@
+//#include"LeetCode.h"
+//using namespace std;
+///*
+//给定一个字符串，逐个翻转字符串中的每个单词。
+//
+//示例 1：
+//
+//输入: "the sky is blue"
+//输出: "blue is sky the"*/
+//string reverseWords(string s) {
+//    istringstream ss(s);
+//    string res;
+//    while (ss >> s) res = " " + s + res;
+//    return res.empty() ? "" : res.substr(1);
+//}
+//int main()
+//{
+//
+//	return 0;
+//}
