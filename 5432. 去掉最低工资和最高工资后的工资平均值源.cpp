7040de70a2f576a96c8a4a9450e1d0455182dfaa
Cdreamfly <2290660986@qq.com>
2020-06-28 11:41:13 +0800

    29双周赛

---
+++ 5432. 去掉最低工资和最高工资后的工资平均值源.cpp
@@ -0,0 +1,23 @@
+﻿//#include"LeetCode.h"
+//using namespace std;
+///*给你一个整数数组 salary ，数组里每个数都是 唯一 的，其中 salary[i] 是第 i 个员工的工资。
+//
+//请你返回去掉最低工资和最高工资以后，剩下员工工资的平均值。 
+//
+//示例 1：
+//
+//输入：salary = [4000,3000,1000,2000]
+//输出：2500.00000
+//解释：最低工资和最高工资分别是 1000 和 4000 。
+//去掉最低工资和最高工资以后的平均工资是 (2000+3000)/2= 2500
+//*/
+//double average(vector<int>& salary) {
+//	sort(salary.begin(), salary.end());
+//	double avg = accumulate(salary.begin() + 1, salary.end() - 1, 0) / (double)(salary.size() - 2);
+//	return avg;
+//}
+//int main()
+//{
+//
+//	return 0;
+//}
---
+++ 5433. n 的第 k 个因子.cpp
@@ -0,0 +1,32 @@
+﻿//#include"LeetCode.h"
+//using namespace std;
+///*给你两个正整数 n 和 k 。
+//
+//如果正整数 i 满足 n % i == 0 ，那么我们就说正整数 i 是整数 n 的因子。
+//
+//考虑整数 n 的所有因子，将它们 升序排列 。请你返回第 k 个因子。如果 n 的因子数少于 k ，请你返回 -1 。
+//
+//示例 1：
+//
+//输入：n = 12, k = 3
+//输出：3
+//解释：因子列表包括 [1, 2, 3, 4, 6, 12]，第 3 个因子是 3 。
+//。*/
+//int kthFactor(int n, int k) {
+//	vector<int>v;
+//	for (int i = 1; i <= n; ++i)
+//	{
+//		if (n % i == 0)
+//		{
+//			v.push_back(i);
+//		}
+//	}
+//	if (v.size() < k)return-1;
+//	sort(v.begin(), v.end());
+//	return v[k - 1];
+//}
+//int main()
+//{
+//	cout<<kthFactor(1, 1);
+//	return 0;
+//}
