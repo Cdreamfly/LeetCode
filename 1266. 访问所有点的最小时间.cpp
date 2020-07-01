f0a68a7c9a626b560ec3a669950419d49cb44f9e
Cdreamfly <2290660986@qq.com>
2020-07-01 21:14:18 +0800

    ...

---
+++ 1266. 访问所有点的最小时间.cpp
@@ -0,0 +1,17 @@
+//#include"LeetCode.h"
+//using namespace std;
+//
+//int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+//	int  res = 0;
+//	for (int i = 0; i < points.size()-1; ++i)
+//	{
+//		res += max(abs(points[i][0] - points[i + 1][0]), abs(points[i][1] - points[i + 1][1]));
+//	}
+//	return res;
+//}
+//
+//int main()
+//{
+//
+//	return 0;
+//}
