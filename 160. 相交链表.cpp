7ef97866c2b883f178aa2638b40953b5bac61517
Cdreamfly <2290660986@qq.com>
2020-07-21 20:59:46 +0800

    走对方的路，浪漫的爱情。

---
+++ 160. 相交链表.cpp
@@ -0,0 +1,42 @@
+#include"LeetCode.h"
+using namespace std;
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+ 
+class Solution {
+public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        if (headA == nullptr || headB == nullptr) return nullptr;
+        ListNode* A = headA;
+        ListNode* B = headB;
+        while (A != B)
+        {
+            if (A == nullptr)
+            {
+                A = headB;
+            }
+            else
+            {
+                A = A->next;
+            }
+            if (B == nullptr)
+            {
+                B = headA;
+            }
+            else
+            {
+                B = B->next;
+            }
+        }
+        return A;
+    }
+};
+int main()
+{
+
+	return 0;
+}
