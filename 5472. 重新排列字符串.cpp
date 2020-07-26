9760a3cd6122474c887da40f02558390344173d9
Cdreamfly <2290660986@qq.com>
2020-07-26 11:33:07 +0800

    31，199

---
+++ 5472. 重新排列字符串.cpp
@@ -0,0 +1,28 @@
+#include"LeetCode.h"
+using namespace std;
+/*
+通过的用户数3173
+尝试过的用户数3256
+用户总通过次数3200
+用户总提交次数3738
+题目难度Easy
+给你一个字符串 s 和一个 长度相同 的整数数组 indices 。
+
+请你重新排列字符串 s ，其中第 i 个字符需要移动到 indices[i] 指示的位置。
+
+返回重新排列后的字符串。*/
+
+string restoreString(string s, vector<int>& indices) {
+	string res = s;
+	for (int i = 0; i < indices.size(); ++i)
+	{
+		res[indices[i]] = s[i];
+	}
+	return res;
+}
+int main()
+{
+
+
+	return 0;
+}
